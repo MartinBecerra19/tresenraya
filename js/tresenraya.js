777f8221a7728e7a3051cce6e6644c1ff6e55650
MartinBecerra19 <martinbecerra1996@gmail.com>
2018-05-03 23:00:05 +0200

    estado final

--- js/tresenraya.js
+++ js/tresenraya.js
@@ -1,77 +1,116 @@
+var contador = 0;
+var juego = ["", "", "", "", "", "", "", "", ""];
+var turno = 0;
+var marcadorX = 0;
+var marcadorO = 0;
 window.onload = function () {
-    var juego = ["", "", "", "", "", "", "", "", ""]
-    var el = document.getElementById("body");
-    el.setAttribute("style", "height:" + window.innerHeight + "px");
-    
+    var el = document.getElementById("body")
+    el.setAttribute("style", "height:" + window.innerHeight + "px")
     var celdas = document.getElementsByClassName("celda");
 
-    let turno = 0;
-    for (const celda of celdas) {
+
+
+    for (let celda of celdas) {
         celda.onclick = function () {
-            if (turno == 0) {
-                celda.innerHTML = "X";
-                turno++;
-                juego[celda.id] = "X"
-                document.getElementById("jugador2").setAttribute("class","seleccionado");
-                document.getElementById("jugador1").setAttribute("class","noseleccionado");
-            } else {
-                celda.innerHTML = "O";
-                turno--;
-                juego[celda.id] = "O";
-                document.getElementById("jugador1").setAttribute("class","seleccionado");
-                document.getElementById("jugador2").setAttribute("class","noseleccionado");
+            if (celda.textContent == "") {
+                if (turno == 0) {
+                    celda.innerHTML = "X"
+                    turno++
+                    contador++
+                    juego[celda.id] = "X"
+                    document.getElementById("jugadorO").setAttribute("class", "seleccionado")
+                    document.getElementById("jugadorX").setAttribute("class", "no seleccionado")
+                } else {
+                    celda.innerHTML = "O"
+                    turno--
+                    contador++
+                    juego[celda.id] = "O"
+                    document.getElementById("jugadorX").setAttribute("class", "seleccionado")
+                    document.getElementById("jugadorO").setAttribute("class", "no seleccionado")
+
+                }
+                Comprobar(juego);
             }
-            Comprobar(juego);
         }
     }
 }
 
 function Comprobar(juego) {
     var celdas = document.getElementsByClassName("celda");
-    var contador = 0;
-    if (juego[0] == juego[1] && juego[1] == juego[2] && juego[0] != "") {
-        alert("Ha ganado: " + juego[0]);
-        Vacia();
+    if ((juego[0] == juego[1]) && (juego[1] == juego[2]) && juego[0] != "") {
+        alert("Ha ganado: " + juego[0])
+        var ganador = juego[0]
+        Puntuar(ganador)
+        Vaciar();
     }
     if (juego[3] == juego[4] && juego[4] == juego[5] && juego[3] != "") {
-        alert("Ha ganado: " + juego[3]);
-        Vacia();
+        alert("Ha ganado: " + juego[3])
+        var ganador = juego[3]
+        Puntuar(ganador)
+        Vaciar();
     }
     if (juego[6] == juego[7] && juego[7] == juego[8] && juego[6] != "") {
-        alert("Ha ganado: " + juego[6]);
-        Vacia();
+        alert("Ha ganado: " + juego[6])
+        var ganador = juego[6]
+        Puntuar(ganador)
+        Vaciar();
     }
     if (juego[0] == juego[3] && juego[3] == juego[6] && juego[0] != "") {
-        alert("Ha ganado: " + juego[0]);
-        Vacia();
+        alert("Ha ganado: " + juego[0])
+        var ganador = juego[0]
+        Puntuar(ganador)
+        Vaciar();
     }
     if (juego[1] == juego[4] && juego[4] == juego[7] && juego[1] != "") {
-        alert("Ha ganado: " + juego[1]);
-        Vacia();
+        alert("Ha ganado: " + juego[1])
+        var ganador = juego[1]
+        Puntuar(ganador)
+        Vaciar();
     }
     if (juego[2] == juego[5] && juego[5] == juego[8] && juego[2] != "") {
-        alert("Ha ganado: " + juego[2]);
-        Vacia();
+        alert("Ha ganado: " + juego[2])
+        var ganador = juego[2]
+        Puntuar(ganador)
+        Vaciar();
     }
     if (juego[0] == juego[4] && juego[4] == juego[8] && juego[0] != "") {
-        alert("Ha ganado: " + juego[0]);
-        Vacia();
+        alert("Ha ganado: " + juego[0])
+        var ganador = juego[0]
+        Puntuar(ganador)
+        Vaciar();
     }
     if (juego[2] == juego[4] && juego[4] == juego[6] && juego[2] != "") {
-        alert("Ha ganado: " + juego[2]);
-        Vacia();
+        alert("Ha ganado: " + juego[2])
+        var ganador = juego[2]
+        Puntuar(ganador)
+        Vaciar();
     }
     if (contador >= 9) {
-        alert("Empate");
-        Vacia();
+        alert("Empate")
+        Vaciar()
     }
-    function Vacia() {
-        var celdas = document.getElementsByClassName("celda");
-        for (let celda of celdas) {
-            celda.innerHTML = "";
-            juego = ["", "", "", "", "", "", "", "", ""];
-            turno = 0;
-            contador = 0;
-        }
+}
+
+function Vaciar() {
+    var celdas = document.getElementsByClassName("celda");
+    for (let celda of celdas) {
+        celda.innerHTML = "";
+        juego = ["", "", "", "", "", "", "", "", ""];
+        contador = 0;
+        turno = 0;
+        document.getElementById("jugadorX").setAttribute("class", "seleccionado")
+        document.getElementById("jugadorO").setAttribute("class", "no seleccionado")
+    }
+}
+
+function Puntuar(ganador) {
+    
+    if (ganador == "X") {
+        marcadorX++
+        document.getElementById("marcadorX").innerHTML = marcadorX;
+    }
+    if (ganador == "O") {
+        marcadorO++
+        document.getElementById("marcadorO").innerHTML = marcadorO;
     }
 }
